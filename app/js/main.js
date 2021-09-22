26643d4e61a4f160dee18b72daecd274d84bb43b
zsline <sergei.dereka@gmail.com>
2021-09-22 20:21:01 +0300

    added section mission

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function () {
+  $('.mission__slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: true,
+    prevArrow: "<img class='a-left control-c prev slick-prev' src='../images/prew-arrow.svg'>",
+    nextArrow: "<img class='a-right control-c next slick-next' src='../images/next-arrow.svg'>"
 
+  });
 })
