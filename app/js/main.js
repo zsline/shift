e8f1715d0400879c0459715cbbbff2cbf69ff021
zsline <sergei.dereka@gmail.com>
2021-09-23 14:13:39 +0300

    added art and about

--- app/js/main.js
+++ app/js/main.js
@@ -10,4 +10,15 @@ $(function () {
     nextArrow: "<img class='a-right control-c next slick-next' src='../images/next-arrow.svg'>"
 
   });
+  $('.about__slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: true,
+    prevArrow: "<img class='a-left control-a prev slick-prev' src='../images/prew-arrow.svg'>",
+    nextArrow: "<img class='a-right control-a next slick-next' src='../images/next-arrow.svg'>"
+
+  });
 })
